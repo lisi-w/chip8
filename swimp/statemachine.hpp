47d37dd7b3fc2289e5cca0f241fc858b27c40052
Daniel Indictor <daniel.indictor@gmail.com>
2022-03-17 23:56:28 -0400

    Finish (but not test) state machine.

--- swimp/statemachine.hpp
+++ swimp/statemachine.hpp
@@ -3,6 +3,7 @@
 
 #include <array>
 #include <cstdint>
+#include <span>
 #include <stack>
 #include <vector>
 
@@ -10,36 +11,48 @@ class statemachine {
 public:
   const static int MEMORY_SIZE = 4096;
   const static int STACK_SIZE = 16;
-  typedef std::array<uint16_t, MEMORY_SIZE / 2> instructions;
-  typedef std::array<uint8_t, MEMORY_SIZE> memory;
-  typedef std::array<uint8_t, 16> regs;
-  typedef uint16_t instruction;
-  typedef uint16_t keystate;
+  const static int DISPLAY_WIDTH = 64;
+  const static int DISPLAY_HEIGHT = 32;
 
   enum status {
     NO_ERROR = 0,
     NOT_IMPLEMENTED = 1,
+    WAITING_FOR_KEYPRESS = 2,
     POPPED_EMPTY_STACK = -2,
     PUSHED_FULL_STACK = -3,
     VFRACE = -4,
+    MEMORY_OVERFLOW = -5,
+    DISPLAY_OVERFLOW = -6,
+    IMPOSSIBLE_KEYPRESS_REQUEST = -6,
   };
 
-  statemachine(instructions imem, instruction pc);
+  statemachine(std::array<uint8_t, MEMORY_SIZE> mem, uint16_t pc,
+               uint16_t font_begin);
 
   /**
    * Executes one instruction
+   * @param ticks Number of 60Hz whole ticks that have passed since the last
+   * invocation.
    */
-  status execute(keystate state);
+  status step(uint16_t keystate, uint32_t ticks);
+
+  /// Get current value of delay timer register.
+  inline uint8_t reg_DT() const { return m_reg_DT; };
+
+  /// Get current value of sound timer register.
+  inline uint8_t reg_ST() const { return m_reg_ST; };
 
 private:
-  union alignas(8 /* Value should be at least 4 */) {
-    memory m_mem;
-    instructions m_imem;
-  };
-  regs m_regs;
-  std::stack<instruction, std::vector<instruction>> m_stack;
-  instruction m_pc;
-  uint8_t m_reg_I;
+  std::array<uint8_t, MEMORY_SIZE> m_mem;
+  std::array<uint8_t, DISPLAY_WIDTH * DISPLAY_HEIGHT> m_display;
+  std::array<uint8_t, 16> m_regs;
+  std::stack<uint16_t, std::vector<uint16_t>> m_stack;
+  uint16_t m_pc;
+  uint16_t m_font_begin;
+  uint16_t m_reg_I;
+  // Timer registers.
+  uint8_t m_reg_DT; // delay timer.
+  uint8_t m_reg_ST; // sound timer
 };
 
 #endif // SWIMP_STATEMACHINE_H
