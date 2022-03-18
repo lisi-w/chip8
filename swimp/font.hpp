cfe29c90373cc0e4d7a36ca2e58d3619ccd1307d
Daniel Indictor <daniel.indictor@gmail.com>
2022-03-17 22:01:18 -0400

    Fix font format.

--- swimp/font.hpp
+++ swimp/font.hpp
@@ -5,12 +5,9 @@
 #include <cstdint>
 
 /*
- * Each glyph is represented as an unsigned 64 bit int. Each row of the glyph is
- * left-aligned to the first 5 bits of each byte. The first row is in the lowest
- * byte, with successive rows in higher bytes.
- *
  * Font specified by http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
  */
-extern const std::array<uint64_t, 16> font;
+const static std::size_t FONT_SPRITE_SIZE = 5;
+extern const std::array<uint8_t, FONT_SPRITE_SIZE * 16> font;
 
 #endif // SWIMP_FONT_H
