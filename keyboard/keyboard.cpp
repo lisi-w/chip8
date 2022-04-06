4fec9fae483d6f1df57ec3b26a0f933165446b05
Elysia Witham <e.witham@columbia.edu>
2022-04-05 21:28:44 -0400

    skeleton

---
+++ keyboard/keyboard.cpp
@@ -0,0 +1,20 @@
+import <iostream>
+
+
+class Keyboard {
+	// define constructor
+	
+	bool find_keyboard() {
+		return false;
+	}
+
+	uint16_t get_keys() {
+		uint16_t key = 0x29;
+		return key;
+	}
+
+	uint16_t get_control_keys() {
+		uint16_t key = 0x29;
+		return key;
+	}
+}
