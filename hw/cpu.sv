cb18fa3a88519cf15ed88a8ebc8ed1d1ce00c483
Daniel Indictor <daniel.indictor@gmail.com>
2022-05-08 21:03:27 -0400

    Finish controller (at least the parts relevant to the display).

--- hw/cpu.sv
+++ hw/cpu.sv
@@ -1,19 +1,19 @@
 module cpu(input logic clk,
+  input logic [15:0] keystate,
   input logic may_run,
 
-  output logic ram_req,  // If we're asking for rw op on any ram.
+  output logic disp_ram_req,  // If we're asking for rw op on display ram.
 
   output logic [7:0] disp_aa, disp_ab, // Addresses
   output logic [7:0] disp_da, disp_db, // Data in
   output logic disp_wa, disp_wb,       // Write Enables
   input logic [7:0] disp_qa, disp_qb,  // Data out
 
-  output logic [7:0] ch_aa, ch_ab,     // Addresses
+  output logic [11:0] ch_aa, ch_ab,     // Addresses
   output logic [7:0] ch_da, ch_db,     // Data in
   output logic ch_wa, ch_wb,           // Write Enables
-  input logic [7:0] ch_qa, ch_qb,      // Data out
+  input logic [7:0] ch_qa, ch_qb       // Data out
 
-  input logic [15:0] keystate
 );
 
 
--- hw/yachip8.sv
+++ hw/yachip8.sv
@@ -11,14 +11,14 @@ module yachip8(input logic clk,
   output logic 	   VGA_SYNC_n);
 
 // Miscellaneous variables.
-logic [15:0] keystate;
+logic [15:0] keystate;  // updated in "control logics" section.
 
 // Note that the word "req" here is shorthand for "request".
 
 //
 // Instantiate rams.
 //
-//
+
 logic [7:0] disp_aa, disp_ab;
 logic [7:0] disp_da, disp_db;
 logic       disp_wa, disp_wb;
@@ -46,19 +46,18 @@ chip_ram ch_ram(.clk,
 // Instantiate cpu
 //
 
-logic cpu_may_run, cpu_ram_req;
-assign cpu_may_run = !write;
-logic [7:0] cpu_disp_aa_req, cpu_disp_ab_req;
-logic [7:0] cpu_disp_da_req, cpu_disp_db_req;
-logic       cpu_disp_wa_req, cpu_disp_wb_req;
-logic [7:0] cpu_ch_aa_req, cpu_ch_ab_req;
-logic [7:0] cpu_ch_da_req, cpu_ch_db_req;
-logic       cpu_ch_wa_req, cpu_ch_wb_req;
+logic cpu_may_run, cpu_disp_ram_req;
+logic  [7:0] cpu_disp_aa_req, cpu_disp_ab_req;
+logic  [7:0] cpu_disp_da_req, cpu_disp_db_req;
+logic        cpu_disp_wa_req, cpu_disp_wb_req;
+logic [11:0] cpu_ch_aa_req, cpu_ch_ab_req;
+logic  [7:0] cpu_ch_da_req, cpu_ch_db_req;
+logic        cpu_ch_wa_req, cpu_ch_wb_req;
 
 cpu ch(.clk, .keystate,
   .may_run(cpu_may_run),
 
-  .ram_req(cpu_ram_req),
+  .disp_ram_req(cpu_disp_ram_req),
   .disp_wa(cpu_disp_wa_req), .disp_wb(cpu_disp_wb_req),
   .disp_da(cpu_disp_da_req), .disp_db(cpu_disp_db_req),
   .disp_aa(cpu_disp_aa_req), .disp_ab(cpu_disp_ab_req),
@@ -66,7 +65,7 @@ cpu ch(.clk, .keystate,
   .ch_wa(cpu_ch_wa_req), .ch_wb(cpu_ch_wb_req),
   .ch_da(cpu_ch_da_req), .ch_db(cpu_ch_db_req),
   .ch_aa(cpu_ch_aa_req), .ch_ab(cpu_ch_ab_req),
-  .ch_qa, .ch_qb,
+  .ch_qa, .ch_qb
 );
 
 //
@@ -83,7 +82,7 @@ display disp(.clk,
 //
 // Control logics
 //
-//
+
 // Logics to control who is touching what.
 // The hierarchy is as follows:
 // 1. the controller may write to any address.
@@ -93,37 +92,62 @@ display disp(.clk,
 // Characterize read/writes.
 // (whether the requested address is the 4k ram, a keystate, etc).
 logic req_addr_is_ch_ram, req_addr_is_disp_ram,
-req_addr_is_keystate, req_addr_is_control;
-assign req_addr_is_ch_ram   = (12'h000 <= address) && (address < 12'h800);
+  req_addr_is_keystate, req_addr_is_control;
+assign req_addr_is_ch_ram   = (address < 12'h800);
 assign req_addr_is_disp_ram = (12'h800 <= address) && (address < 12'h880);
-assign req_addr_is_ch_ram   = (12'h880 == address);
+assign req_addr_is_keystate = (12'hFFE == address);
 assign req_addr_is_control  = (12'hFFF == address);
+
+logic [11:0] address_to_ch_ram_lower_addr, address_to_ch_ram_upper_addr;
+assign address_to_ch_ram_lower_addr = address << 1;
+assign address_to_ch_ram_upper_addr = address << 1 | 12'b1;
+logic [7:0] address_to_disp_ram_lower_addr, address_to_disp_ram_upper_addr;
+assign address_to_disp_ram_lower_addr = {1'b0, address[6:0]};
+assign address_to_disp_ram_upper_addr = {1'b0, address[6:0]} << 1 | 8'b1;
 
 // Delegate ram access to display, ram, or user of this module.
 always_comb begin
-  if (write) begin
-    disp_wa = 0;
-    disp_wb = 0;
-    ch_wa = 0;
-    ch_wb = 0;
+  cpu_may_run = 0;
+  {disp_aa, disp_ab, disp_da, disp_db, disp_wa, disp_wb} = 0;
+  {ch_aa, ch_ab, ch_da, ch_db, ch_wa, ch_wb} = 0;
 
-    if (req_addr_is_ch_ram) begin
-
-    end else if (req_addr_is_disp_ram) begin
-    end else if (req_addr_is_ch_ram) begin
-    end
-
-
-  end else if (cpu_ram_req) begin
-
+  if (write && req_addr_is_ch_ram) begin
+    ch_aa = address_to_ch_ram_lower_addr;
+    ch_ab = address_to_ch_ram_upper_addr;
+    ch_da = writedata[15:8];
+    ch_da = writedata[7:0];
+    {ch_wa, ch_wb} = 2'b11;
+  end else if (write && req_addr_is_disp_ram) begin
+    disp_aa = address_to_disp_ram_lower_addr;
+    disp_ab = address_to_disp_ram_upper_addr;
+    disp_da = writedata[15:8];
+    disp_da = writedata[7:0];
+    {disp_wa, disp_wb} = 2'b11;
   end else begin
+    // If the controller isn't writing, cpu can do what it wants.
+    cpu_may_run = 1;
+    // If the controller isn't writing, cpu controls ch_ram.
+    {ch_aa, ch_ab} = {cpu_ch_aa_req, cpu_ch_ab_req};
+    {ch_da, ch_db} = {cpu_ch_da_req, cpu_ch_db_req};
+    {ch_wa, ch_wb} = {cpu_ch_wa_req, cpu_ch_wb_req};
+
+    // display only gets disp_ram if cpu isn't asking for it.
+    if (cpu_disp_ram_req) begin
+      {disp_aa, disp_ab} = {cpu_disp_aa_req, cpu_disp_ab_req};
+      {disp_da, disp_db} = {cpu_disp_da_req, cpu_disp_db_req};
+      {disp_wa, disp_wb} = {cpu_disp_wa_req, cpu_disp_wb_req};
+    end else begin
+      disp_aa = display_addr_req;
+    end
   end
 end
 
-logic cpu_go, cpu_reset;
-logic cpu_writing;
 
-// TODO: Set cpu_may_run
+initial keystate = 0;
+always_ff @(posedge clk) begin
+  if (write && req_addr_is_keystate) begin
+    keystate <= writedata;
+  end
+end
 
-
-  endmodule
+endmodule
