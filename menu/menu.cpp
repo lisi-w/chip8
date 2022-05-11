661ffeeb239311e6f9731176fd9fa0494e51e958
Elysia Witham <e.witham@columbia.edu>
2022-05-11 12:52:13 -0400

    changed for our implementation

--- menu/menu.cpp
+++ menu/menu.cpp
@@ -5,7 +5,7 @@
  * Name/UNI: Elysia Witham (ew2632)
  */
 #include "keyboard.hpp"
-#include "fpga_ram.h"
+#include "chip8.h"
 #include <iostream>
 #include <cstdlib>
 #include <sys/ioctl.h>
@@ -30,27 +30,10 @@
 
 int chip8_fd;
 
-void print_mem(unsigned char addr) {
-  fpga_ram_arg_t real_vla;
-  real_vla.address = addr;
-  real_vla.data = 0x04;
-  if (ioctl(chip8_fd, FPGA_RAM_READ, &real_vla)) {
-      perror("ioctl(FPGA_RAM_READ) failed");
-      return;
-  }
-  printf("%02x\n",
-	real_vla.data);
-
-}
-
-/* Set the background color */
-void set_mem(fpga_ram_arg_t *vla)
+void set_mem(unsigned int instr)
 {
-  fpga_ram_arg_t real_vla;
-  real_vla.address = vla->address;
-  real_vla.data = vla->data;
-  if (ioctl(chip8_fd, FPGA_RAM_WRITE, &real_vla)) {
-      perror("ioctl(FPGA_RAM_WRITE) failed");
+  if (ioctl(chip8_fd, CHIP8_INSTR_WRITE, &instr)) {
+      perror("ioctl(CHIP8_INSTR_WRITE) failed");
       return;
   }
 }
@@ -65,42 +48,39 @@ int main()
     exit(1);
   }
   std::cout << "Keyboard initialized\n";
+  unsigned int chip8_instr;
   
   /* set up chip 8 userspace */
-  static const char filename[] = "";
-  std::cout << "CHIP 8 Userspace program started\n";
-  fpga_ram_arg_t my_mem_args;
-  my_mem_args.data = 0x07;
-  my_mem_args.address = 0x07;
+  static const char filename[] = "/dev/chip8";
+  std::cout << "CHIP8 Userspace program started\n";
 
   if ( (chip8_fd = open(filename, O_RDWR)) == -1) {
     std::cerr << "could not open file\n";
     return -1;
   }
-  memset(&my_mem_args, 0, sizeof(fpga_ram_arg_t));
 
   /* Look for and handle keypresses */
   for (;;) {
     Keyboard::keys keys = keyboard_obj.get_keys();
     if (keys.game1) {
       std::cout << "Game 1 selected\n";
-      print_mem(GAME1_INSTR);
+      set_mem(GAME1_INSTR);
     }
     else if (keys.game2) {
       std::cout << "Game 2 selected\n";
-      print_mem(GAME2_INSTR);
+      set_mem(GAME2_INSTR);
     }
     else if (keys.game3) {
       std::cout << "Game 3 selected\n";
-      print_mem(GAME3_INSTR);
+      set_mem(GAME3_INSTR);
     }
     else if (keys.game4) {
       std::cout << "Game 4 selected\n";
-      print_mem(GAME4_INSTR);
+      set_mem(GAME4_INSTR);
     }
     else if (keys.game5) {
       std::cout << "Game 5 selected\n";
-      print_mem(GAME5_INSTR);
+      set_mem(GAME5_INSTR);
     }
     std::cout << (int)keys.keypad << std::endl;
   }
